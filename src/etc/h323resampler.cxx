05d3acc62aa7afe87b5739c7a5cab3c3081e6497
shorne89 <s.horne@packetizer.com>
2016-02-01 09:56:21 +1000

    New speex resampling module

---
+++ src/etc/h323resampler.cxx
@@ -0,0 +1,29 @@
+/*
+* h323resampler.cxx
+*
+* Copyright (c) 2015 Spranto International Pte Ltd. All Rights Reserved.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, you can obtain one at http://mozilla.org/MPL/2.0/
+*
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+* the License for the specific language governing rights and limitations
+* under the License.
+*
+* Contributor(s): ______________________________________.
+*
+*
+*/
+
+#include <h323.h>
+
+#ifdef H323_RESAMPLE
+
+#include <etc/h323resampler.h>
+#include <speex/speex_resampler.h>
+
+
+
+#endif  // H323_RESAMPLE
